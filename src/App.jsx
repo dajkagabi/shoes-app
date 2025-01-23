c762793b5168cbb8f8e04ae5b513a069d529ac70
dajkagabi <dajkagabi01@gmail.com>
2025-01-23 16:27:23 +0100

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./components/Home/Home";
 import Products from "./components/Products/Products";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Route path="/about" element={<About/>}/>
       <Route path="/contact" element={<Contact/>}/>
     </Routes>
+    <Footer/>
     </BrowserRouter>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+
+
+const Footer = () => {
+  return (
+    <footer className="bg-white shadow-md text-black py-4">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
+        <p>&copy; 2025 Shoes-App. All rights reserved.</p>
+        
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
